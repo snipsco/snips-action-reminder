71dc59c43298b24cc95656f2475dbaa1bdf33a7d
CoorFun <coorfang@gmail.com>
2019-02-19 17:55:20 +0100

    [Change] enable all output

--- action.js
+++ action.js
@@ -2,9 +2,6 @@
 const debug = require('debug')
 const { name } = require('./package.json')
 
-// Enable error print
-debug.enable(name + ':error')
-// Uncomment this line to print everything
-debug.enable(name + ':info')
+debug.enable(name + ':*')
 
 require('./src/index')()
