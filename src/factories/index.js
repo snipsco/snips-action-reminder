e86996e68a114ccd8b5fed448ff6aab467f9efea
CoorFun <coorfang@gmail.com>
2019-02-26 17:26:22 +0100

    [Change] remove remindersFactory

--- src/factories/index.js
+++ src/factories/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     configFactory: require('./configFactory'),
     i18nFactory: require('./i18nFactory'),
-    httpFactory: require('./httpFactory'),
-    reminderFactory: require('./reminderFactory')
+    httpFactory: require('./httpFactory')
 }
