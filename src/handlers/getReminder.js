9bbd4552c4395fadc6d852cf23a2c2a208213770
CoorFun <coorfang@gmail.com>
2019-02-28 15:32:07 +0100

    [Change] super nice and clean getReminder handler now :collision:

--- src/handlers/getReminder.js
+++ src/handlers/getReminder.js
@@ -2,98 +2,45 @@ const { logger, arrayIntersection, generateMessageForReminders } = require('../u
 const i18nFactory = require('../factories/i18nFactory')
 const commonHandler = require('./common')
 const {
-    getAllComingReminders,
-    getAllExpiredReminders,
-    getRemindersByDatetime,
-    getRemindersByName,
-    getRemindersByRecurrence
+    getReminders
 } = require('../reminders')
 
-module.exports = async function (msg, flow, knownSlots) {
+module.exports = async function (msg, flow) {
     logger.debug('getReminder')
     const i18n = i18nFactory.get()
-    const slots = await commonHandler(msg, knownSlots)
+    const slots = await commonHandler(msg)
+    const reminders = getReminders(
+        slots.reminder_name,
+        slots.datetime,
+        slots.recurrence,
+        slots.past_reminders ? true : false
+    )
 
-    const reminders = (slots.past_reminders) ? getAllExpiredReminders() : getAllComingReminders()
-
-    // there is no reminders, slots not provided
-    if (!(reminders.length || slots.datetime || slots.reminder_name || slots.recurrence)) {
+    // No reminders, no slots
+    if (!reminders && !Object.keys(slots).length) {
+        logger.debug('No reminders, no slots')
         flow.end()
-        return i18n(slots.past_reminders ? 'inform.noPastReminder' : 'inform.noComingReminder')
+        return i18n('getReminder.info.noReminderFound')
     }
 
-    // there is no reminders, slots provided, continue to ask if user need to create
-    if (!reminders.length && !slots.past_reminders && Object.keys(slots).length) {
+    // No reminders, slots detected
+    if (!reminders && Object.keys(slots).length) {
+        logger.debug('No reminders, slots detected')
         flow.continue('snips-assistant:Yes', (msg, flow) => {
-            return require('./index').setReminder(msg, flow)
+            slots.depth = 3
+            return require('./index').setReminder(msg, flow, slots)
         })
         flow.continue('snips-assistant:No', (msg, flow) => {
             flow.end()
         })
-        return i18n('inform.noComingReminder')+' '+i18n('ask.create')
+        return i18n('getReminder.info.noReminderFound') + i18n('setReminder.ask.createReminder')
     }
 
-    // there are reminders, no slots provided
-    if (reminders.length && !Object.keys(slots).length) {
+    // Found reminders by using some of the constrains
+    if (reminders) {
+        logger.debug('Found reminders by using some of the constrains')
         flow.end()
         return generateMessageForReminders(reminders, slots.past_reminders ? true : false)
-    }
-
-    // there are reminders, only past_reminders slots provided
-    if (reminders.length && Object.keys(slots).length === 1 && slots.past_reminders) {
-        flow.end()
-        return generateMessageForReminders(reminders, true)
-    }
-
-    // there are reminders, past_reminders slots provided, other slots provided
-    if (reminders.length && Object.keys(slots).length > 1 && slots.past_reminders) {
-        flow.end()
-
-        let res = null
-
-        const remindersFromDatetime = slots.datetime ? getRemindersByDatetime(slots.datetime) : []
-        const remindersFromName = slots.reminder_name ? getRemindersByName(slots.reminder_name) : []
-
-        // recurrence remonder will never be past, always non-expired
-        //const remindersFromRecurrence = slots.recurrence ? getRemindersByRecurrence(slots.recurrence) : []
-
-        if (remindersFromDatetime.length && remindersFromName.length) {
-            res = arrayIntersection(remindersFromDatetime, remindersFromName)
-        } else {
-            res = remindersFromDatetime.concat(remindersFromName)
-        }
-
-        return res.length ? generateMessageForReminders(res, true) : i18n('inform.noReminderFound')
-    }
-
-    //there are reminders, past_reminders slots not provided, other slots provided
-    if (reminders.length && Object.keys(slots).length && !slots.past_reminders) {
-        flow.end()
-
-        let res = null
-
-        const remindersFromDatetime = slots.datetime ? getRemindersByDatetime(slots.datetime) : []
-        const remindersFromName = slots.reminder_name ? getRemindersByName(slots.reminder_name) : []
-        const remindersFromRecurrence = slots.recurrence ? getRemindersByRecurrence(slots.recurrence) : []
-
-        logger.debug(`reminder found by name ${remindersFromName}`)
-        logger.debug(`reminder found by datetime ${remindersFromDatetime}`)
-        logger.debug(`reminder found by recurrence ${remindersFromRecurrence}`)
-
-        if (remindersFromDatetime.length && remindersFromName.length && remindersFromRecurrence.length) {
-            res = arrayIntersection(remindersFromDatetime, remindersFromName)
-            res = arrayIntersection(remindersFromRecurrence, res)
-        } else if (!remindersFromDatetime.length && remindersFromName.length && remindersFromRecurrence.length) {
-            res = remindersFromName.concat(remindersFromRecurrence)
-        } else if (remindersFromDatetime.length && !remindersFromName.length && remindersFromRecurrence.length) {
-            res = remindersFromDatetime.concat(remindersFromRecurrence)
-        } else if (remindersFromDatetime.length && remindersFromName.length && !remindersFromRecurrence.length) {
-            res = remindersFromDatetime.concat(remindersFromName)
-        } else {
-            res = remindersFromName.concat(remindersFromDatetime).concat(remindersFromRecurrence)
-        }
-
-        return res.length ? generateMessageForReminders(res) : i18n('inform.noReminderFound')
     }
 
     flow.end()
