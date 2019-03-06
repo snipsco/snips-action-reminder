9d6b6625d2c92ea0c4a7448719a8f54d2d2795bd
CoorFun <coorfang@gmail.com>
2019-03-06 19:03:01 +0100

    [Add] more tts templates

--- src/constants.js
+++ src/constants.js
@@ -12,18 +12,5 @@ module.exports = {
     INTENT_PROBABILITY_THRESHOLD: 0.2,
     INTENT_FILTER_PROBABILITY_THRESHOLD: 0,
     SLOT_CONFIDENCE_THRESHOLD: 0,
-    ASR_TOKENS_CONFIDENCE_THRESHOLD: 0.2,
-    RECURRENCE_TTS_MAP: {
-        'mondays': 'every monday',
-        'tuesdays': 'every tuesday',
-        'wednesdays': 'every wednesday',
-        'thursdays': 'every thursday',
-        'fridays': 'every friday',
-        'saturdays': 'every saturday',
-        'sundays': 'every sunday',
-        'monthly': 'every month',
-        'daily': 'every day',
-        'weekly': 'every week',
-        'weekends': 'weekend'
-    }
+    ASR_TOKENS_CONFIDENCE_THRESHOLD: 0.2
 }
