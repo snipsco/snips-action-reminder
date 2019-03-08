78f2be879de3a854d3f9e3b501afa2d4aa6d9d3b
CoorFun <coorfang@gmail.com>
2019-03-08 16:32:50 +0100

    [Change] use reuse getReminder handler for searching

--- src/handlers/cancelReminder.js
+++ src/handlers/cancelReminder.js
@@ -1,4 +1,5 @@
-const { logger, arrayIntersection, generateMessageForReminders } = require('../utils')
+const { logger, arrayIntersection } = require('../utils')
+const generateReport = require('../tts/generateReport')
 const i18nFactory = require('../factories/i18nFactory')
 const { extractSlots, flowContinueBuiltin } = require('./common')
 //const generateDatetimeTTS = require('../tts/generateDatetimeTTS')
@@ -8,63 +9,61 @@ const {
     getReminders
 } = require('../reminders')
 
-module.exports = async function (msg, flow) {
+module.exports = async function (msg, flow, knownSlots = { depth: 2}, reminders = null ) {
     logger.debug('cancelReminder')
     const i18n = i18nFactory.get()
     const slots = await extractSlots(msg)
-    const reminders = getReminders(
-        slots.reminder_name,
-        slots.datetime,
-        slots.recurrence,
-        slots.past_reminders ? true : false
-    )
 
-    // No reminders
-    if (!reminders.length) {
-        logger.debug('No reminders')
-        flow.end()
-        return i18n('getReminders.info.noReminderFound')
+    if (!reminders) {
+        return require('./index').getReminder(msg, flow, { depth: 3}, (msg, flow, knownSlots, reminders) => {
+            return require('./index').cancelReminder(msg, flow, knownSlots, reminders)
+        })
     }
+
+    const report = generateReport(reminders, slots)
 
     // Cancel all the found reminders
-    if (reminders.length && slots.all_reminders) {
-        logger.debug('Cancel all the found reminders')
-        flow.continue('snips-assistant:Yes', (msg, flow) => {
-            flow.end()
-            reminders.forEach(reminder => {
-                deleteReminderById(reminder.id)
-            })
-            return i18n('cancelReminder.info.confirmRemindersCancelled')
+    //if (reminders) {
+    logger.debug('Cancel all the found reminders')
+    flow.continue('snips-assistant:Yes', (msg, flow) => {
+        flow.end()
+        reminders.forEach(reminder => {
+            deleteReminderById(reminder.id)
         })
-        flow.continue('snips-assistant:No', (msg, flow) => {
-            flow.end()
-        })
-        return i18n('getReminders.info.foundReminders', {
-            number: reminders.length,
-            odd: (reminders.length === 1) ? '' : 's'
-        }) + i18n ('cancelReminder.ask.confirmToCancelAll')
-    }
+        return (reminders.length === 1) ? i18n('cancelReminder.info.successfullyCancelled_', {
+            name: reminders[0].name
+        }) : i18n('cancelReminder.info.confirmRemindersCancelled')
+    })
+    flow.continue('snips-assistant:No', (msg, flow) => {
+        flow.end()
+    })
+
+    return reminders.length === 1 ? i18n('cancelReminder.ask.confirmToCancelReminder', {
+        name: reminders[0].name,
+        time: getTimeHuman(reminders[0].datetime, reminders[0].recurrence)
+    }) : report.head + i18n('cancelReminder.ask.confirmToCancelAll')
+    //}
 
     // Found reminders by using some of the constrains
-    if (reminders.length === 1) {
-        flow.continue('snips-assistant:Yes', (msg, flow) => {
-            flow.end()
-            deleteReminderById(reminders[0].id)
-            return i18n('cancelReminder.info.confirmRemindersCancelled', {
-                name: reminders[0].name
-            })
-        })
-        flow.continue('snips-assistant:No', (msg, flow) => {
-            flow.end()
-        })
-        return i18n('cancelReminder.ask.confirmToCancelReminder', {
-            name: reminders[0].name,
-            datetime: generateDatetimeTTS(reminders[0].datetime)
-        })
-    } else {
-        flow.end()
-        return i18n('cancelReminder.info.multiRemindersFound')
-    }
+    // if (reminders.length === 1) {
+    //     flow.continue('snips-assistant:Yes', (msg, flow) => {
+    //         flow.end()
+    //         deleteReminderById(reminders[0].id)
+    //         return i18n('cancelReminder.info.successfullyCancelled_', {
+    //             name: reminders[0].name
+    //         })
+    //     })
+    //     flow.continue('snips-assistant:No', (msg, flow) => {
+    //         flow.end()
+    //     })
+    //     return i18n('cancelReminder.ask.confirmToCancelReminder', {
+    //         name: reminders[0].name,
+    //         time: getTimeHuman(reminders[0].datetime, reminders[0].recurrence)
+    //     })
+    // } else {
+    //     flow.end()
+    //     return i18n('cancelReminder.info.multiRemindersFound')
+    // }
 
     flow.end()
     return i18n('debug.caseNotRecognized')
