5e41fc56be1e95aadca8d0684f75d848cf3964f9
CoorFun <coorfang@gmail.com>
2019-04-18 18:28:04 +0200

    enhanced camelize function

--- src/utils/camelize.ts
+++ src/utils/camelize.ts
@@ -1,7 +1,18 @@
 export const camelize = {
-    camelize: (string: string) => string.replace(/_\w/g, snakePart =>
-        snakePart[1].toUpperCase()
-    ),
+    /**
+     * Convert all non-camelized name to camelized format
+     * 
+     * Examples: 
+     *      SetPokemon -> setPokemon
+     *      set_Pokemon -> setPokemon
+     *      set Pokemon -> setPokemon
+     *      set-Pokemon -> setPokemon
+     */
+    camelize: (string: string): string => {
+        return string.replace(/^([A-Z])|[\s-_](\w)/g, function(match, p1, p2, offset) {
+            return p2 ? p2.toUpperCase() : p1.toLowerCase()  
+        });
+    },
     camelizeKeys: (obj: {[ key: string ]: any }) => {
         const clone: {[ key: string ]: any } = {}
         for(let key in obj){
