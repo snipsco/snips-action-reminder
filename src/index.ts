d5bf869be4ae9cbbc12e8bb8bba41a1569abce19
CoorFun <coorfang@gmail.com>
2019-04-18 18:27:27 +0200

    subscribe to all the intent

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { withHermes } from 'hermes-javascript'
 import bootstrap from './bootstrap'
-import handlers from './handlers'
-import { translation, logger } from './utils'
+import { configFactory } from './factories'
+import { translation, logger, camelize } from './utils'
+import { CONFIDENCE_DEFAULT, INTENT_PREFIX_DEFAULT, INTENTS_MAIN } from './constants'
+import handlers, { HandlerOptions } from './handlers'
 
-// Initialize hermes
 export default function ({
     hermesOptions = {},
     bootstrapOptions = {}
@@ -11,13 +12,28 @@ export default function ({
     return new Promise((resolve, reject) => {
         withHermes(async (hermes, done) => {
             try {
-                // Bootstrap config, locale, i18n…
                 await bootstrap(bootstrapOptions)
+                
+                const config = configFactory.get()
+                // Construct handler options
+                const handlerOptions: HandlerOptions = {
+                    confidenceScore: {
+                        intentStandard: Number(config.confidenceIntentStanderd) || CONFIDENCE_DEFAULT.INTENT_STANDARD,
+                        intentDrop: Number(config.confidenceIntentDrop) || CONFIDENCE_DEFAULT.INTENT_DROP,
+                        slotDrop: Number(config.confidenceSlotDrop) || CONFIDENCE_DEFAULT.SLOT_DROP,
+                        asrDrop: Number(config.confidenceAsrDrop) || CONFIDENCE_DEFAULT.ASR
+                    }
+                }
 
                 const dialog = hermes.dialog()
-
-                // This is a placeholder! Replace that by something valid!
-                dialog.flow('pokemon', handlers.pokemon)
+                // Subscribe to non-elicitation intent
+                INTENTS_MAIN.forEach( (intent) => {
+                    dialog.flow(
+                        `${config.intentPrefix || INTENT_PREFIX_DEFAULT}${intent}`, 
+                        (msg, flow) => handlers[camelize.camelize(intent)](msg, flow, hermes, handlerOptions)
+                    )
+                })
+    
                 resolve(done)
             } catch (error) {
                 // Output initialization errors to stderr and exit
