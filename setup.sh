916254f031626f26e50797a67f2ab9d011a5503c
CoorFun <coorfang@gmail.com>
2019-04-18 11:01:31 +0200

    re-init the repo with ts

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# File names definition
+CONFIG_FILE_DEFAULT="config.ini.default"
+CONFIG_FILE="config.ini"
+DB=".db_reminders"
+
+# Install dependencies and compile typescript
+npm install && npm run build
+
+# Check user configuration file
+if [ ! -e ${CONFIG_FILE} ]
+then
+    cp ${CONFIG_FILE_DEFAULT} ${CONFIG_FILE}
+fi
+
+# Check reminder database folder
+if [ ! -e ${DB} ]
+then
+    echo "created db folder"
+    mkdir ${DB}
+else
+    echo "found db folder"
+fi
