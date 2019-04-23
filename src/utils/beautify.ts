79618097ac06043ad94678242d87024bbe9d6e42
CoorFun <coorfang@gmail.com>
2019-04-23 19:21:44 +0200

    add datetime for beautifier

--- src/utils/beautify.ts
+++ src/utils/beautify.ts
@@ -2,20 +2,19 @@ import { configFactory, i18nFactory } from '../factories'
 import { LANGUAGE_MAPPINGS } from '../constants'
 import moment from 'moment'
 
-export const beautify = {    
+export const beautify = {
     date: (date: Date): string => {
         const i18n = i18nFactory.get()
         const config = configFactory.get()
         const language = LANGUAGE_MAPPINGS[config.locale]
 
         return moment(date).locale(language).calendar(undefined, {
-            sameDay: i18n('moment.sameDay'),
-            nextDay: i18n('moment.nextDay'),
-            nextWeek: i18n('moment.nextWeek'),
-            sameElse: i18n('moment.sameElse'),
-        })
+            sameDay: i18n('moment.date.sameDay'),
+            nextDay: i18n('moment.date.nextDay'),
+            nextWeek: i18n('moment.date.nextWeek'),
+            sameElse: i18n('moment.date.sameElse'),
+        }).replace(' 0', '')
     },
-
     time: (date: Date): string => {
         const i18n = i18nFactory.get()
         const config = configFactory.get()
@@ -23,7 +22,19 @@ export const beautify = {
 
         return moment(date)
             .locale(language)
-            .format(i18n('moment.time'))
+            .format(i18n('moment.time.12H'))
             .replace(' 0', '')
-    }
+    },
+    datetime: (date: Date): string => {
+        const i18n = i18nFactory.get()
+        const config = configFactory.get()
+        const language = LANGUAGE_MAPPINGS[config.locale]
+
+        return moment(date).locale(language).calendar(undefined, {
+            sameDay: i18n('moment.datetime.sameDay'),
+            nextDay: i18n('moment.datetime.nextDay'),
+            nextWeek: i18n('moment.datetime.nextWeek'),
+            sameElse: i18n('moment.datetime.sameElse'),
+        }).replace(' 0', '')
+    },
 }
