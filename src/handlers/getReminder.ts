ec05272647259d9b069e83d6a83ff81182946163
CoorFun <coorfang@gmail.com>
2019-04-29 18:23:12 +0200

    getReminder handler

--- src/handlers/getReminder.ts
+++ src/handlers/getReminder.ts
@@ -1,7 +1,50 @@
-import { Handler } from './index'
-import { logger } from '../utils'
+import handlers, { Handler } from './index'
+import { logger, translation } from '../utils'
+import { flowContinueTerminate, nextOptions, ReminderSlots, extractSltos } from './common';
+import { Reminder } from '../class/Reminder';
 
 export const getReminderHandler: Handler = async function (msg, flow, database, options) {
-    logger.debug('getReminder')
+    logger.debug(`${msg.intent.intentName}, depth: ${options.depth}`)
+
+    flowContinueTerminate(flow)
+
+    // Add handler for intentNotRecognized
+    flow.notRecognized((_, flow) => {
+        return handlers.setReminder(msg, flow, database, nextOptions(options, slots))
+    })
+
+    // Extract slots
+    const slots: ReminderSlots = extractSltos(msg, options)
+    
+    // Get reminders
+    const reminders: Reminder[] = database.get({
+        isExpired: false,
+        name: slots.reminderName || undefined,
+        datetimeRange: slots.datetimeRange || undefined,
+        recurrence: slots.recurrence || undefined
+    })
+    logger.debug('found: ', reminders)
+    // No reminders, no slots
+    if (!reminders.length && !Object.keys(slots).length) {
+        flow.end()
+        return translation.random('getReminder.info.noReminderFound')
+    }
+
+    // No reminders, slots detected
+    if (!reminders.length && Object.keys(slots).length) {
+        flow.continue('snips-assistant:Yes', (msg, flow) => {
+            return handlers.setReminder(msg, flow, database, nextOptions(options, slots))
+        })
+        return translation.random('getReminders.info.noSuchRemindersFound') + 
+               translation.random('setReminder.ask.createReminder')
+    }
+
+    // Found reminders by using some of the constrains, no need to continue
+    if (reminders.length) {
+        flow.end()
+        return
+    }
+
     flow.end()
+    throw new Error('setReminderUnhandled')
 }
