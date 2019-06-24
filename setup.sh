2005a4a4368bb1f257cbd3f9e40f9f265104bce9
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-06-24 10:41:04 +0200

    Generic format

--- setup.sh
+++ setup.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 
-# File names definition
-CONFIG_FILE_DEFAULT="config.ini.default"
-CONFIG_FILE="config.ini"
-DB=".db"
-
-# Install dependencies and compile typescript
 npm install && npm run build
 
-# Check user configuration file
-if [ ! -e ${CONFIG_FILE} ]
+if [ ! -e "./config.ini" ]
 then
-    cp ${CONFIG_FILE_DEFAULT} ${CONFIG_FILE}
-fi
-
-# Check reminder database folder
-if [ ! -e ${DB} ]
-then
-    echo "created db folder"
-    mkdir ${DB}
-else
-    echo "found db folder"
+    cp config.ini.default config.ini
 fi
