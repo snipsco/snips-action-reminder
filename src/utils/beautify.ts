e4c659fabfc668ef46fd1617b15a95b7ddcac593
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-07-11 16:47:47 +0200

    Updated legacy code

--- src/utils/beautify.ts
+++ src/utils/beautify.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import moment from 'moment'
+import 'moment/locale/fr'
 import { config, i18n } from 'snips-toolkit'
+import { DateRange } from './parser'
 import { ASSETS_DIR } from '../constants'
 
 export type DateFormats = {[key: string]: any}
@@ -20,12 +22,14 @@ export const beautify = {
             sameElse: dateFormats.moment.date.sameElse
         }).replace(' 0', '')
     },
+
     time: (date: Date): string => {
         return moment(date)
             .locale(config.get().locale)
             .format(dateFormats.moment.time.format12H)
             .replace(' 0', '')
     },
+
     datetime: (date: Date): string => {
         return moment(date).locale(config.get().locale).calendar(undefined, {
             sameDay: dateFormats.moment.datetime.sameDay,
@@ -34,20 +38,22 @@ export const beautify = {
             sameElse: dateFormats.moment.datetime.sameElse
         }).replace(' 0', '')
     },
-    daterange: (datetimeRange: DatetimeRange): string => {
-        if ((new Date(datetimeRange.min)).getDay() === 5 && new Date(datetimeRange.max).getDay() === 1) {
+
+    daterange: (dateRange: DateRange): string => {
+        if ((new Date(dateRange.min)).getDay() === 5 && new Date(dateRange.max).getDay() === 1) {
             return dateFormats.moment.daterange.weekEnd
         }
 
-        if (new Date(datetimeRange.min).getDay() === 1 && new Date(datetimeRange.max).getDay() === 1) {
+        if (new Date(dateRange.min).getDay() === 1 && new Date(dateRange.max).getDay() === 1) {
             return dateFormats.moment.daterange.nextWeek
         }
 
         return i18n.translate('daterange.fromTo', {
-            dateMin: beautify.date(new Date(datetimeRange.min)),
-            dateMax: beautify.date(new Date(datetimeRange.max - 1000))
+            dateMin: beautify.date(new Date(dateRange.min)),
+            dateMax: beautify.date(new Date(dateRange.max.getTime() - 1000))
         })
     },
+
     recurrence: (date: Date, recurrence: string): string => {
         if (recurrence === 'daily') {
             return dateFormats.moment.recurrence.daily
